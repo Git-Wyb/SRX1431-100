47cc25d282e0963e4425799b0bf82c1254990aaf
xiang <xiangquan3721@163.com>
2018-08-14 14:04:53 +0800

    Bunka STM8L151G6 RX Source Ver1.9.0 hu_Internal VCO

--- user_src/ram.c
+++ user_src/ram.c
@@ -79,7 +79,17 @@ UINT8 SIO_buff[16];
 UINT8 SIO_DATA[16];
 UINT8 Tx_Rx_mode=0;
 ADF70XX_REG_T ROM_adf7012_value[16];
-const ADF70XX_REG_T Default_adf7012_value[16]={0x00000000,0x031B5011,0x00000000,0x00000000,
+
+//External Inductor VCO
+//const ADF70XX_REG_T Default_adf7012_value[16]={0x00000000,0x031B5011,0x00000000,0x00000000,
+//                                               0x00000000,0x00000000,0x00000000,0x00000000,
+//                                               0x00000000,0x00000000,0x00000000,0x00000000,
+//                                               0x00000000,0x00000000,0x00000000,0x00000000,
+//                                               };
+
+
+//Internal Inductor VCO 
+const ADF70XX_REG_T Default_adf7012_value[16]={0x00000000,0x01535011,0x00000000,0x00000000,     
                                                0x00000000,0x00000000,0x00000000,0x00000000,
                                                0x00000000,0x00000000,0x00000000,0x00000000,
                                                0x00000000,0x00000000,0x00000000,0x00000000,
